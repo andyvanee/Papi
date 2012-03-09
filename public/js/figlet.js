2400989975bc54b278de30324a6fb6a3656c44d0
Andy Vanee <1andyvanee@gmail.com>
2012-03-09 00:48:10 -0700

    Add some browser-side controls and realtime updating.

--- public/js/figlet.js
+++ public/js/figlet.js
@@ -4,6 +4,15 @@ $(document).ready(function(){
       $('#font').append('<option value="'+elem+'">'+elem+'</option>');
     });
   });
+  
+  $('#figlet_form textarea').bind('keyup', function(){
+    $('#figlet_form').trigger('submit');
+  });
+  
+  $('#figlet_form select').bind('change', function(){
+    $('#figlet_form').trigger('submit');
+  });
+  
   $('#figlet_form').submit(function(ev){
     ev.preventDefault();
     $.get('/api.php?'+$(this).serialize(), function(data){
@@ -12,4 +21,15 @@ $(document).ready(function(){
       }
     });
   });
+  
+  $('#browser_font').bind('change', function(){
+    var font = $(this).val();
+    var menlo = (font == 'Menlo Bold')
+    $('#output').css({'font-family': menlo?'Menlo':font, 'font-weight': menlo?'bold':'normal'});
+  });
+
+  $('#browser_font_size').bind('change', function(){
+    $('#output').css({'font-size': $(this).val()});
+  });
+  
 });
